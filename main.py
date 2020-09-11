99ba4204619dec52fc262f663a3c68dbadb5b34c
congnguyendinh0 <congnguyendinh0@gmail.com>
2020-09-11 17:36:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+list = [1,2,8,4,5]
+listTwo = [1,'two',3,True,5]
+names = ['cong','bong','long']
+
+
+#print(listTwo[0])
+
+#names.append('garry')
+#names.insert(0,'lisa')
+#names.remove('cong')
+#names.reverse()
+
+list.sort()
+print(names)
+print(list)
+
+for i in list:
+  print(i)
